4f2fb062bfa3c3bb6677001064275e7dd8acd275
Müller <luka-nima.mueller@adesso.de>
2024-05-03 09:44:32 +0200

    table update

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,7 +1,8 @@
 -- Creation of table
 CREATE TABLE IF NOT EXISTS students (
   matrikl_nr INT NOT NULL,
-  course_id INT NOT NULL,
+  student_course_id INT,
+  module_course_id INT,
   name varchar(250) NOT NULL,
   first_name varchar(250) NOT NULL,
   dob DATE,
