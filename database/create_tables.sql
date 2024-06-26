57b57c8f48e33b51b6545d50457f97a22471f543
NimaMueller <lukanimamani.mueller@gmail.com>
2024-06-26 11:33:15 +0200

    final

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -2,18 +2,18 @@
 CREATE TABLE IF NOT EXISTS students (
   matr_nr INT NOT NULL,
   student_course_id INT,
-  module_course_id INT,
+  --module_course_id INT,
   name varchar(250) NOT NULL,
   first_name varchar(250) NOT NULL,
   dob DATE,
-  active_modules INTEGER ARRAY,
-  passed_modules INTEGER ARRAY,
-  failed_modules INTEGER ARRAY,
+  active_modules VARCHAR(250) ARRAY,
+  passed_modules VARCHAR(250),
+  failed_modules VARCHAR(250),
   PRIMARY KEY (matr_nr)
 );
 
 CREATE TABLE IF NOT EXISTS modules (
-  module_id INT NOT NULL,
+  module_id VARCHAR(250) NOT NULL,
   description varchar(250) NOT NULL,
   c_p INT NOT NULL,
   PRIMARY KEY (module_id)
@@ -23,13 +23,13 @@ CREATE TABLE if NOT EXISTS courses (
     course_id INT NOT NUll,
     description VARCHAR(250) NOT NULL,
     degree VARCHAR(250) NOT NULL,
-    module_list INTEGER ARRAY,
+    module_list VARCHAR(250) ARRAY,
     PRIMARY KEY (course_id)
 );
 
 CREATE TABLE if NOT EXISTS signUp (
   student_matr_nr INT NOT NULL,
-  module_id INT NOT NULL,
+  module_id VARCHAR(250) NOT NULL,
   grade DOUBLE PRECISION,
   passed BOOLEAN,
   attempt INT NOT NULL,
